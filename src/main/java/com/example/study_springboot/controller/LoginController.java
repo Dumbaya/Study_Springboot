32a1f29101edb75ae642a3496b2f745ec1b8cfff
wkrns9 <wkrns9@naver.com>
2024-01-31 17:02:36 +0900

    html 간단한 틀, 마리아db 사용 준비, Entity 설계하기

--- src/main/java/com/example/study_springboot/controller/LoginController.java
+++ src/main/java/com/example/study_springboot/controller/LoginController.java
@@ -2,7 +2,7 @@ package com.example.study_springboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+//
 @Controller
 public class LoginController {
     @GetMapping("/login")
